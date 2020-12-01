f090ee4d0123d934ed8ea90886ee14b58ab57ac3
subsonic16 <subsonic16@outlook.com>
2020-12-01 20:28:58 +0000

    Added comments

--- CarLoan.java
+++ CarLoan.java
@@ -1,30 +1,49 @@
+//Programming 12 - Car Loan
+/*
+@Trevor Liu
+
+@12/01/2020
+
+Program summary
+This program takes in 4 inputs. The car loan price, the length in years, monthly interest rate, and downpayment. 
+It validates the loan, and calculates how much monthly is needed.
+*/
+
 public class CarLoan{
+  //Global Variables
   double carLoan;
   double loanLength;
   double interestRate;
   double downPayment;
     
 
-  public CarLoan(double loanP, double lengthP, double interestP, double downPaymentP){
+  public CarLoan(double loanP, double lengthP, double interestP, double downPaymentP){ //Constructor 
     carLoan = loanP;
     loanLength = lengthP;
     interestRate = interestP;
     downPayment = downPaymentP;
-
-  }
-
+  } //End of constructor
 
 
-  public static boolean checkLoan(double loan){
+
+  //checkLoan function
+  //Returns a boolean
+  //checks to see if the loan is valid
+  public static boolean checkLoan(double loan){ 
     if (loan > 0){
       return true;
     } else {
       System.err.println("Yikes man");
       System.out.println(loan);
       return false;
-    }
-  }
+    }//end of if/else
+  }//end of checkLoan function
 
+
+
+  //checkLoanLength function
+  //Returns boolean
+  //checks to see if loan length is valid 
   public static boolean checkLoanLength(double loan){
     if (loan > 0){
       return true;
@@ -32,9 +51,14 @@ public class CarLoan{
       System.err.println("Yikers man");
       System.out.println(loan);
       return false;
-    }
-  }
+    }//end of if/else
+  }//end of checkLoan function
 
+
+
+  //checkDownPayment function
+  //returns a boolean
+  //checks to see if the downpayment is smaller than the loan
   public static boolean checkDownPayment(double loan, double DownPay){
     if (DownPay < loan){
       return true;
@@ -44,15 +68,26 @@ public class CarLoan{
     }
   }  
 
+
+
+  //findMonthlyPayment function
+  //returns monthly payment
+  //Calculates the monthly payment
   public double findMonthlyPayment(){
    
-    double monthlyBalance = (carLoan-downPayment)/(loanLength*12);
-    double interest = monthlyBalance*interestRate/100;
+    double monthlyBalance = (carLoan-downPayment)/(loanLength*12); //finds monthly balance
+    double interest = monthlyBalance*interestRate/100; //finds interest
 
-    return monthlyBalance + interest;
+    return monthlyBalance + interest; //returns the total
   }
 
+
+
+  //calculate function
+  //parameter {CarLoan Object} - Car to be loaned
+  //checks to see if loan is valid, and finds monthly payment
   public static void calculate(CarLoan loan){
+
     if(checkLoan(loan.carLoan)&&checkLoanLength(loan.loanLength)&&checkDownPayment(loan.carLoan, loan.downPayment)){
 
       System.out.println("Successful Loan!!");
@@ -60,12 +95,11 @@ public class CarLoan{
     }
   }
 
+  //Main
   public static void main(String[] args){
-    CarLoan Chevy = new CarLoan(10000, 3, 5, 2000);
-    calculate(Chevy);
+
+    CarLoan Chevy = new CarLoan(10000, 3, 5, 2000);//Creates instance
+    calculate(Chevy); //runs calculate function
 
   }
-
-
-
 }
